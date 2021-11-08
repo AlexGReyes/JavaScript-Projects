ecda49b2b6cd9cc1019c966dce436ed99c563366
Alejandro reyes <agreyesaldeco@gmail.com>
2021-11-08 10:22:09 -0800

    added files

---
+++ Basic JavaScript Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,23 @@
+
+
+var A = 'This is an example';                                   // defining a A-variable and giving it a string value
+document.write(A);                                              // displaying A-vaiable
+
+window.alert(A);                                                // displaying A-varibale in an alert box
+
+
+var B = ' I don\'t like';                                       // defining a B-variable and giving it a string value
+window.alert(B);                                                // displaying B-varibale in an alert box
+
+var C = A + B;                                                  // defining a C-Var and giving it a A-var and B-var concatenation
+
+
+document.write(C);                                              // displaying C-var
+
+var name=" Alejandro ", lastname = " Reyes ", color =" black ";       // defining 3 string variables
+
+name = name.fontcolor("blue");                                  // using the fontcolor method on name variable
+lastname = lastname.fontcolor("red");                           // using the fontcolor method on lastname variable
+color = color.fontcolor("green");                               // using the fontcolor method on color variable
+
+document.write(name + lastname + color);                                           // displaying name variable
